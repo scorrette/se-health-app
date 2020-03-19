0fed835b704f0ece4c9ad771bc7b5cc0998665ce
Ariela Chomski <ariela.chomski@gmail.com>
2020-03-19 14:44:40 -0400

    Removed unnecessary comments/imports

--- App.js
+++ App.js
@@ -9,28 +9,10 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {
-  createStackNavigator,
-  createAppContainer,
-} from '@react-navigation/stack';
 import {Component} from 'react';
 import Login from './Login';
 import CreateAccount from './CreateAccount';
 import RootStack from './RootStack';
-import HowYouDoin from './HowYouDoin';
-// const AppNavigator = createStackNavigator(
-//   {
-//     LoginScreen: {
-//       screen: Login,
-//     },
-//     CreateAccountScreen: {
-//       screen: CreateAccount,
-//     },
-//   },
-//   {
-//     initialRouteName: 'LoginScreen',
-//   },
-// );
 
 export default function App() {
   return (
